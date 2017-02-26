eddf8cf763cef5cf1b0d1394cb9a00c9cda58189
Dmitriy Butakov <dmitry404@gmail.com>
2017-02-26 23:50:16 +0200

    Smoke tests for Cassandra cluster (requires a started cassandra container)

---
+++ week1-cassandra/src/test/java/bdata/week1/ClusterSmokeTest.java
@@ -0,0 +1,27 @@
+package bdata.week1;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ClusterSmokeTest {
+  @Test
+  public void checkIfRunning() {
+    try (Cluster cluster = Cluster.builder()
+        .addContactPoint("127.0.0.1")
+        .build()) {
+      Session session = cluster.connect();
+
+      ResultSet rs = session.execute("select release_version from system.local");
+      Row row = rs.one();
+
+      assertThat(row.getString("release_version"), is("3.10"));
+    }
+  }
+}
