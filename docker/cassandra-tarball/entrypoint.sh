32217e68df675c096048680fcdee5fff3e85512d
Dmitriy Butakov <dmitry404@gmail.com>
2017-02-27 19:58:11 +0200

    you can now run nodes in cluster using --seed IP

--- docker/cassandra-tarball/entrypoint.sh
+++ docker/cassandra-tarball/entrypoint.sh
@@ -2,13 +2,20 @@
 
 echo "\nConfiguring Cassandra...\n"
 
+CASSANDRA_CONFIG=$CASSANDRA_DIR/conf
 HOSTNAME_ADDR="$(hostname --ip-address)"
-CASSANDRA_CONFIG=$1/conf
+
+if [ "$1" = "--seed" ] && [ -n "$2" ]; then
+    SEED_ADDR=$2
+else
+    SEED_ADDR=$HOSTNAME_ADDR
+fi
 
 sed -ri 's/('listen_address':).*/\1 '"$HOSTNAME_ADDR"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
 sed -ri 's/# ('broadcast_address':)(.*)/\1 '"$HOSTNAME_ADDR"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
 sed -ri 's/# ('broadcast_rpc_address':)(.*)/\1 '"$HOSTNAME_ADDR"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
-sed -ri 's/(- seeds:).*/\1 "'"$HOSTNAME_ADDR"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+
+sed -ri 's/(- seeds:).*/\1 "'"$SEED_ADDR"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
 
 sed -ri 's/^(# )?('rpc_address':).*/\2 '0.0.0.0'/' "$CASSANDRA_CONFIG/cassandra.yaml"
 
