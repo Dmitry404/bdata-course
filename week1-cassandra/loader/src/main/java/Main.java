119d14f9ef6c51091d5d89920a02218af8866d37
Dmitriy Butakov <dmitry404@gmail.com>
2017-03-04 00:03:25 +0200

    a simple entry point

---
+++ week1-cassandra/loader/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    //todo add acceptance tests
+    // - a smoke test for cassandra cluster
+    // - a smoke test for the main
+  }
+}
