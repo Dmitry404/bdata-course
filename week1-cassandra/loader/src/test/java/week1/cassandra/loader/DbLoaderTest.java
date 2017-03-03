4837c82959a32d06515385d0dc37c57ba692f9d2
Dmitriy Butakov <dmitry404@gmail.com>
2017-03-03 15:12:37 +0200

    changes to DBLoader tests - setup/cleanup

--- week1-cassandra/loader/src/test/java/week1/cassandra/loader/DbLoaderTest.java
+++ week1-cassandra/loader/src/test/java/week1/cassandra/loader/DbLoaderTest.java
@@ -5,28 +5,43 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class DbLoaderTest {
+  private Cluster cluster;
+
+  @Before
+  public void setUp() throws Exception {
+    cluster = Cluster.builder()
+        .addContactPoint("127.0.0.1")
+        .build();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    if (!cluster.isClosed()) {
+      cluster.close();
+    }
+  }
 
   @Test
-  public void main() throws Exception {
-    try (Cluster cluster = Cluster.builder()
-        .addContactPoint("127.0.0.1")
-        .build()) {
-      Session session = cluster.connect();
+  public void mainFlow() throws Exception {
+    DbLoader loader = new DbLoader(cluster.newSession(), new TweetsSupplier("oneLineTweet.csv"));
+    loader.truncateDb();
+    loader.populateDb();
 
-      DbLoader loader = new DbLoader(cluster.newSession(), new TweetsSupplier("oneLineTweet.csv"));
-      loader.truncateDb();
-      loader.populateDb();
+    Session session = cluster.connect();
+    ResultSet rs = session.execute("select * from bdcourse.tweets");
+    Row row = rs.one();
 
-      ResultSet rs = session.execute("select * from bdcourse.tweets");
-      Row row = rs.one();
+    assertThat(row.getString("id"), is("837277950202490885"));
 
-      assertThat(row.getString("id"), is("837277950202490885"));
-    }
+    session.close();
+    loader.truncateDb();
   }
 }
