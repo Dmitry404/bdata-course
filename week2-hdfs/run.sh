957a751dbb34cadee489b7c639b4d4cec42302f1
Dmitriy Butakov <dmitry404@gmail.com>
2017-03-26 11:56:10 +0300

    a small fix of diag message in run.sh

--- week2-hdfs/run.sh
+++ week2-hdfs/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Running java -jar week2-hdfs.jar $@"
+echo "Running java -jar week2-hdfs.jar --host hdfs-master-node $@"
 java -jar $HOME/week2-hdfs.jar --host hdfs-master-node "$@"
