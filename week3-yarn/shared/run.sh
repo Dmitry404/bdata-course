1436faf54aa5a5090d9114ae1a150de388bf53db
Dmitriy Butakov <dmitry404@gmail.com>
2017-04-07 18:30:29 +0300

    upload input.data on each run (for testing purposes)

--- week3-yarn/shared/run.sh
+++ week3-yarn/shared/run.sh
@@ -5,15 +5,15 @@ cd /root/app
 echo "Trying to cleanup hdfs:/output directory"
 hdfs dfs -rm -r /output
 echo "Copying input to hdfs:/input.dat"
-hdfs dfs -put input.dat /input.dat
+hdfs dfs -put -f input.dat /input.dat
 
 echo "Running yarn jar week3-hdfs.jar $@"
 yarn jar week3-yarn.jar "$@"
 
 if [ $? -eq 0 ]
 then
-  echo "Payments Input:"
-  hdfs dfs -text /input.dat
+  echo "Payments Input (truncated to the first 20 lines if the total size is more than that):"
+  hdfs dfs -text /input.dat | head -n 20
 
   echo ""
   echo "Payments Output:"
